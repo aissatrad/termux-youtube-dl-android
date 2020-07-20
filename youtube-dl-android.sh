1ff31847616fc657bc0e0834f17330cc1c02b97f
TRAD Aissa <aissa.trad@fst.utm.tn>
2020-07-20 20:10:39 +0100

    Update youtube-dl-android.sh

--- youtube-dl-android.sh
+++ youtube-dl-android.sh
@@ -9,6 +9,4 @@ mkdir ~/.config/youtube-dl;
 touch ~/.config/youtube-dl/config;
 echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=720]"' > ~/.config/youtube-dl/config;
 mkdir ~/bin;
-#cd ~/bin
-#nano termux-url-opener
-echo 'termux-url-opener' > ~/bin/termux-url-opener;
+echo 'youtube-dl $1' > ~/bin/termux-url-opener;
