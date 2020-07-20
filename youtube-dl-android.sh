863e5c7c6a51c76d85cd1601e002050823feb684
TRAD Aissa <aissa.trad@fst.utm.tn>
2020-07-20 18:58:46 +0100

    Update youtube-dl-android.sh

--- youtube-dl-android.sh
+++ youtube-dl-android.sh
@@ -1,14 +1,17 @@
 #apt update && apt upgrade
 #apt install nano 
-pkg install python
-pip install youtube-dl
-termux-setup-storage
-mkdir /data/data/com.termux/files/home/storage/shared/Youtube
-mkdir -p ~/.config/youtube-dl
+pkg install python;
+echo "Python installed";
+pip install youtube-dl;
+echo "youtube-dl installed";
+termux-setup-storage;
+echo "setup-storage OK";
+mkdir /data/data/com.termux/files/home/storage/shared/Youtube;
+mkdir -p ~/.config/youtube-dl;
 #nano  ~/.config/youtube-dl
 #code
-echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=720]"' > ~/.config/youtube-dl
-mkdir ~/bin
+echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=720]"' > ~/.config/youtube-dl;
+mkdir ~/bin;
 #cd ~/bin
 #nano termux-url-opener
-echo 'termux-url-opener' > ~/bin/termux-url-opener
+echo 'termux-url-opener' > ~/bin/termux-url-opener;
