abac0e89e52bea451667c0e31c4b4b3a26a0e5cf
TRAD Aissa <aissa.trad@fst.utm.tn>
2020-07-20 17:41:13 +0100

    Add files via upload
    Upload code file

---
+++ youtube-dl-android.sh
@@ -0,0 +1,19 @@
+apt update && apt upgrade -y
+
+#apt install nano -y
+apt install python -y
+pip install youtube-dl
+
+termux-setup-storage
+
+mkdir /data/data/com.termux/files/home/storage/shared/Youtube
+mkdir -p ~/.config/youtube-dl
+#nano  ~/.config/youtube-dl
+#code
+echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=720]"' > ~/.config/youtube-dl
+
+mkdir ~/bin
+#cd ~/bin
+#nano termux-url-opener
+
+echo 'termux-url-opener' > ~/bin/termux-url-opener
