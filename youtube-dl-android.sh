9459bddf1028438780ad3af34f4c9a7c64208d31
TRAD Aissa <aissa.trad@fst.utm.tn>
2020-07-20 19:08:17 +0100

    Update youtube-dl-android.sh

--- youtube-dl-android.sh
+++ youtube-dl-android.sh
@@ -1,15 +1,11 @@
-#apt update && apt upgrade
-#apt install nano 
+pkg update ;
+pkg upgrade -y;
 pkg install python -y;
-echo "Python installed";
 pip install youtube-dl;
-echo "youtube-dl installed";
 termux-setup-storage;
-echo "setup-storage OK";
 mkdir /data/data/com.termux/files/home/storage/shared/Youtube;
-mkdir -p ~/.config/youtube-dl;
-#nano  ~/.config/youtube-dl
-#code
+mkdir ~/.config
+touch ~/.config/youtube-dl;
 echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s -f "best[height<=720]"' > ~/.config/youtube-dl;
 mkdir ~/bin;
 #cd ~/bin
